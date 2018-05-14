57a56280d4ec956de6bf66560c1791af0b978781
Waqas Nasir <waqasnasir818@gmail.com>
2018-05-15 00:34:12 +0500

    router added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import {BrowserRouter, Route} from "react-router-dom";
 import "normalize.css/normalize.css"
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp options={[]}/>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        this page is from expense dash board
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        this is a add expense page
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        this is edit page
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is help page
+    </div>
+);
+
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+        </div>
+
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
